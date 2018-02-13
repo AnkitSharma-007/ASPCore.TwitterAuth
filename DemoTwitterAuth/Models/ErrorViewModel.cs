b9ed42154f27db8485f5040b7a187005d87779a6
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2018-02-13 23:40:13 +0530

    First commit

---
+++ DemoTwitterAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoTwitterAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
